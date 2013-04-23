6323666e8187b602d72eb8066ef258bda633a35f
sethau <std0003@tigermail.auburn.edu>
2013-04-23 14:54:25 -0500

    Update Form.java
    Rev1 - added comments

--- Forms/Form.java
+++ Forms/Form.java
@@ -1,5 +1,5 @@
 public abstract interface Form {
-  public abstract void init();
-  public abstract Object getInfo();
-  public abstract void displayInfo(String id);
+  public abstract void init();//perform any initializations
+  public abstract Object getInfo();//display form to read in info from user and return object containing new info
+  public abstract void displayInfo(String id);//display an object's information (by id) to the user
 }
