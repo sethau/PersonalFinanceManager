1a7a045f8c177694293cbd07f39e9eca345813b0
sethau <std0003@tigermail.auburn.edu>
2013-04-23 14:27:15 -0500

    Update Controller.java
    Rev1 - specify Controller interface methods

--- Controllers/Controller.java
+++ Controllers/Controller.java
@@ -1,5 +1,5 @@
-public abstract interface Controller {
-  public abstract Controller() {
-    //ctor
-  }
+public abstract interface Controller {  
+  public abstract boolean create(String name, String type);
+  public abstract boolean remove(String name);
+  public abstract Object get(String name);
 }
