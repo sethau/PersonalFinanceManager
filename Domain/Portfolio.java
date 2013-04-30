987322ac64467c8b2d3ab92b464e42d2fd1b3cb2
Devin Broaden <dbroaden92@gmail.com>
2013-04-29 20:53:18 -0500

    basic Portfolio methods

--- Domain/Portfolio.java
+++ Domain/Portfolio.java
@@ -1,5 +1,32 @@
 package Domain;
 
+import java.util.ArrayList;
+
 public class Portfolio {
 
+	private double balance;
+	private ArrayList<Trade> trades;
+	
+	public Portfolio(double balance) {
+		this.balance = balance;
+		updateTrades();
+	}
+	
+	public Portfolio() {
+		this(0);
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	
+	public ArrayList<Trade> getTrades() {
+		return trades;
+	}
+	
+	public boolean updateTrades() {
+		
+		
+		return true;
+	}
 }
